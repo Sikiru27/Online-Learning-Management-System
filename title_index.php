aa59dc7f47b3f59bf95a115e642599c23f8af46a
Sikiru Kilani <43989230+Sikiru27@users.noreply.github.com>
2018-10-10 15:47:14 +0100

    Add files via upload

---
+++ title_index.php
@@ -0,0 +1,37 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/Logo.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<h2>OYO STATE COLLEGE OF AGRICULTURE & TECH.</h2>
+							<h3>
+							<p>E - Learning</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>OYSCATECH :</p>
+												<p>FOOD Sufficiency, Self Employement and Innovation</p>
+												<p></p>
+								</div>		
+						</div>		
+				</div>
